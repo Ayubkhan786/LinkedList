dcf29f379163fbe84beeae1e4767ed54d6a060e4
your Name <mohamedabdulayubm@gmail.com>
2022-06-28 13:53:05 +0530

    CreateLinkedList

--- LinkedList_DataStructure/Program.cs
+++ LinkedList_DataStructure/Program.cs
@@ -1,2 +1,12 @@
-﻿
+﻿using LinkedList;
+
+
 Console.WriteLine("Welcome to LinkedList");
+
+
+
+linkedlist linklist = new linkedlist();
+linklist.Add(50);
+linklist.Add(36);
+linklist.Add(70);
+linklist.Display();
