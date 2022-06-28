4a8d6357f51bba306432ff8adb19166296cfee47
your Name <mohamedabdulayubm@gmail.com>
2022-06-28 16:18:31 +0530

    Created Using Stack

--- LinkedList_DataStructure/Program.cs
+++ LinkedList_DataStructure/Program.cs
@@ -10,8 +10,15 @@ Console.WriteLine("Welcome to LinkedList");
 
 
 linkedlist linklist = new linkedlist();
-linklist.Add(50);
-linklist.Add(36);
 linklist.Add(70);
+linklist.Add(30);
+linklist.Add(56);
 linklist.Display();
+
+
+Stack stack = new Stack();
+stack.StackPush(70);
+stack.StackPush(30);
+stack.StackPush(56);
+stack.DisplayStack();
 
