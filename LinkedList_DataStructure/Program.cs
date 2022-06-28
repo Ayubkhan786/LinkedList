f99ee545fb4651b4b94a29242fcbd0e5e90c46a0
your Name <mohamedabdulayubm@gmail.com>
2022-06-28 17:11:18 +0530

    modified the data

--- LinkedList_DataStructure/Program.cs
+++ LinkedList_DataStructure/Program.cs
@@ -9,9 +9,9 @@ Console.WriteLine("Welcome to LinkedList");
 
 
 linkedlist linklist = new linkedlist();
-linklist.Add(70);
-linklist.Add(30);
 linklist.Add(56);
+linklist.Add(30);
+linklist.Add(70);
 linklist.Display();
 
 
